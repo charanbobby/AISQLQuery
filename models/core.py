a3c0fc19ce023327fd976cefbe4c786525798356
anandrajB <anand98.ar@gmail.com>
2023-06-25 21:10:32 +0530

    updated

--- models/core.py
+++ models/core.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.core.cache import cache
-
+from abc import ABC , ABCMeta , abstractmethod
 
 
 def make_a_cache_to_models(self):
@@ -8,8 +8,14 @@ def make_a_cache_to_models(self):
     return None
 
 
+@abstractmethod
 def some(self):
     return None
 
 
+
+def new_class_var(self , val):
+    self.val = val
+    return val
+    
 
