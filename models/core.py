3b6ec772080e312d79c26b343a83fe1eb1e89b65
anandrajB <anand98.ar@gmail.com>
2023-06-25 21:07:27 +0530

    updated core

---
+++ models/core.py
@@ -0,0 +1,15 @@
+from django.shortcuts import get_object_or_404
+from django.core.cache import cache
+
+
+
+def make_a_cache_to_models(self):
+    models_cache = get_object_or_404("accounts.value" , pk = 1 )    
+    return None
+
+
+def some(self):
+    return None
+
+
+
